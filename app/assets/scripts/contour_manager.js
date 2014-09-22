815c27f03ecca3ed6e3f414b08c08b713b74319e
john <john@john-Laptop.(none)>
2014-09-22 18:12:08 -0400

    started work on hamburger

--- app/assets/scripts/contour_manager.js
+++ app/assets/scripts/contour_manager.js
@@ -5,17 +5,64 @@ var components = {};
 
 
 // Header component
-components.header = {
-  mobile : function ($this, x, y) {
-    $this.hide();
+components['nav-header'] = {
+
+  mobile : function ($header) {
+    // fade out the links
+    var $links = $header.find('a:not(.logo)');
+    $links.fadeOut('fast', function () {
+
+      // add the header if necesary
+      if (!$header.find('#hamburger').length) {
+        $header.append('<div id="hamburger" display="none"></div><div id="slider"></div>');
+      }
+
+      // fade the hamburger in
+      $header.find('#hamburger').fadeIn('fast', function () {
+        registerHbgrListener();
+console.log('registering listener');
+$('#hamburger').click(function () {
+  console.log('test')
+  $(this).toggleClass('open');
+});
+      });
+    });
   },
-  desktop : function ($this, x, y) {
-    $this.show();
+
+  desktop : function ($header) {
+    // fade the hamburger out
+    $header.find('#hamburger').fadeOut('fast', function () {
+      var $links = $header.find('a:not(.logo)');
+
+      // fade the links in
+      $links.fadeIn('fast', function () {
+        
+      });
+    });
   }
 }
 
 //
-// execute the contour constructor
+// execute the contour constructor with custom states
 //
 
-new contour(null, components);
+var states = {
+  mobile  : 0,
+  desktop : 700
+}; 
+
+new contour(states, components);
+
+//
+//
+// Listeners for basic functionality
+//
+//
+
+function registerHbgrListener () {
+  $('#hamburger').on('click', function () {
+    console.log('test')
+    $(this).toggleClass('open');
+  });
+}
+
