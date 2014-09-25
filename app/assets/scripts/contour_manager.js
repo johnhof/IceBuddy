9e5767cc8cadad4b4c81a3327aa7d3aca198f267
john <john@john-Laptop.(none)>
2014-09-25 19:56:09 -0400

    transitioning responsive code to use angular over jquery (JS DOES NOT COMPILE)

--- app/assets/scripts/contour_manager.js
+++ app/assets/scripts/contour_manager.js
@@ -1,68 +1,72 @@
-//
-// set of contour components
-//
-var components = {};
+// //
+// // set of contour components
+// //
+// var components = {};
 
 
-// Header component
-components['nav-header'] = {
+// // Header component
+// components['nav-header'] = {
 
-  mobile : function ($header) {
-    // fade out the links
-    var $links = $header.find('a:not(.logo)');
-    $links.fadeOut('fast', function () {
+//   mobile : function ($header) {
+//     console.log('firing mobile')
+//     // fade out the links
+//     var $links = $header.find('a:not(.logo)');
+//     console.log($links.length)
+//     $links.fadeOut('fast', function () {
 
-      // add the header if necesary
-      if (!$header.find('#hamburger').length) {
-        $header.append('<div id="hamburger" display="none"></div><div id="slider"></div>');
-      }
+//       // add the header if necesary
+//       if (!$header.find('#hamburger').length) {
+//         $header.append('<div id="hamburger" display="none"></div><div id="slider"></div>');
+//       }
 
-      // fade the hamburger in
-      $header.find('#hamburger').fadeIn('fast', function () {
-        registerHbgrListener();
-console.log('registering listener');
-$('#hamburger').click(function () {
-  console.log('test')
-  $(this).toggleClass('open');
-});
-      });
-    });
-  },
+//       // fade the hamburger in
+//       $header.find('#hamburger').fadeIn('fast', function () {
+//         registerHbgrListener();
+// $('#hamburger').click(function () {
+//   console.log('test')
+//   $(this).toggleClass('open');
+// });
+//       });
+//     });
+//   },
 
-  desktop : function ($header) {
-    // fade the hamburger out
-    $header.find('#hamburger').fadeOut('fast', function () {
-      var $links = $header.find('a:not(.logo)');
+//   desktop : function ($header) {
+//     // fade the hamburger out
+//     $header.find('#hamburger').fadeOut('fast', function () {
+//       var $links = $header.find('a:not(.logo)');
 
-      // fade the links in
-      $links.fadeIn('fast', function () {
+//       // fade the links in
+//       $links.fadeIn('fast', function () {
         
-      });
-    });
-  }
-}
+//       });
+//     });
+//   }
+// }
 
-//
-// execute the contour constructor with custom states
-//
+// //
+// // execute the contour constructor with custom states
+// //
 
-var states = {
-  mobile  : 0,
-  desktop : 700
-}; 
+// var states = {
+//   mobile  : 0,
+//   desktop : 700
+// }; 
 
-new contour(states, components);
+// $.noConflict();
+// jQuery(document).ready(function ($) { 
+//   new contour(states, components);
+// }
 
-//
-//
-// Listeners for basic functionality
-//
-//
+// //
+// //
+// // Listeners for basic functionality
+// //
+// //
 
-function registerHbgrListener () {
-  $('#hamburger').on('click', function () {
-    console.log('test')
-    $(this).toggleClass('open');
-  });
-}
+// function registerHbgrListener () {
+//   $('#hamburger').on('click', function () {
+//     console.log('test')
+//     $(this).toggleClass('open');
+//   });
+// }
 
