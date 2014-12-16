7a5d83c3aac192a221a896b917729eb3fb66f874
Duane Garber <duane@brandingbrand.com>
2014-12-15 22:40:46 -0500

    Adding Functionality for Player CRUD, added Player Unit Tests

--- api/components/players/players_ctrl.js
+++ api/components/players/players_ctrl.js
@@ -1,28 +1,34 @@
 var Err      = require(process.cwd() + '/api/lib/error').errorGenerator;
 var Mongoman = require(process.cwd() + '/api/lib/mongoman');
-
+var Joi      = require('joi');
+var validate = require(process.cwd() + '/api/lib/validate');
 var Player = Mongoman.model('player');
 
-module.exports = function accountController (api) {
+module.exports = function playerController (api) {
   return {
 
     //
     // Create
     //
     create : function (req, res, next) {
-      Mongoman.save('player', req.body, next, function () {
-        var inputs = req.body;
-        // validate(inputs, {
-          
-        // }
+      var inputs = req.body;
 
-        res.data = {
-          success : true,
-          message : 'Player ' + inputs.username + ' created'
+      validate(inputs, {
+        name     : Joi.object().keys({
+          first : Joi.string().optional().alphanum().min(1).max(50),
+          last  : Joi.string().optional().alphanum().min(1).max(50)
+        })
+      }, [
+        function save (error, callback) {
+          Mongoman.save('player', req.body, next, function () {
+            res.data = {
+              success : true,
+              message : 'Player ' + inputs.name.first + ' ' + inputs.name.last + ' created'
+            }
+            return callback();
+          });
         }
-
-        return next();
-      });
+      ], next);
     },
 
 
@@ -30,10 +36,31 @@ module.exports = function accountController (api) {
     // Read
     //
     read : function (req, res, next) {
-      res.data = {
-        success: true
-      };
-      return next();
+      var inputs = req.body;
+      if (inputs && inputs.name && inputs.name.first && inputs.name.last ) {
+        Player.findOne({
+            name : 
+              { 
+                first : inputs.name.first,
+                last : inputs.name.last,
+              }
+          }, function(err, player){
+            if ( player ) {
+              return res.send( { 
+                  success : true, 
+                  player : player 
+                } );
+            } else {
+              return res.send( { 
+                  success : false, 
+                  message : 'No player matched the name: ' + inputs.name.first + ' ' + inputs.name.last
+              } );
+            }
+          });
+
+      } else {
+        return next( Err('Name.First and Name.Last are required fields') );
+      }
     },
 
 
@@ -41,7 +68,29 @@ module.exports = function accountController (api) {
     // Update
     //
     update : function (req, res, next) {
-      return next();
+      var inputs = req.body;
+      if (inputs && inputs.id && inputs.update ) {
+        Player.findOneAndUpdate({
+            _id : inputs.id
+          }, 
+          inputs.update, //fields to Update
+          function(err, player){
+            if ( player ) {
+              //TODO: Add a check to see if it actually updated???
+              return res.send( { 
+                  success : true, 
+                  player : player
+              } );
+            } else {
+              return res.send( { 
+                  success : false, 
+                  message : 'No player matched the id: ' + inputs.id
+              } );
+            }
+          });
+      } else {
+        return next( Err('Player Id and Update Object are required fields') ); 
+      }
     },
 
 
@@ -49,7 +98,28 @@ module.exports = function accountController (api) {
     // Destroy
     //
     destroy : function (req, res, next) {
-      return next();
+      var inputs = req.body;
+      if (inputs && inputs.id ) {
+        Player.findOneAndRemove({
+            _id : inputs.id
+          }, 
+          function(err, player){
+            if ( player ) {
+              //TODO: Add a check to see if it actually updated???
+              return res.send( { 
+                  success : true, 
+                  player : player
+              } );
+            } else {
+              return res.send( { 
+                  success : false, 
+                  message : 'No player matched the id: ' + inputs.id
+              } );
+            }
+          });
+      } else {
+        return next( Err('Player Id is required fields') ); 
+      }
     }
   };
 }
