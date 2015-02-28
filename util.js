8fcad7955b5901393776b4cc71ea202875769387
Duane Garber <duane@brandingbrand.com>
2015-02-28 17:11:37 -0500

    I dont know what Im doing here

--- util.js
+++ util.js
@@ -1,11 +1,9 @@
 var async = require('async');
 var Mon = require('mongoman');
-var helpers = require(process.cwd() + '/api/lib/helpers');
-
-helpers.requireDirContent(process.cwd() + '/api/components/', /.+_model/)
+Mon.registerAll(__dirname + '/api/components', /_model$/i);
 //Utility Functions
-var Player = Mon.model('player');
-var mongoose        = require('mongoose');
+var Player   = Mon.model('player');
+var mongoose = require('mongoose');
 
 // if ( process.argv[2] ) {
 //   switch(process.argv[2]):
@@ -18,92 +16,116 @@ populateDemoData();
 // }
 
 
+function createPlayer ( player, callback ) {
+  console.log('NADE IT?')
+  if ( player && player.name && player.preferred_number ) {
+    console.log('1')
+    Player.create(player, function(error, data){
+      console.log('2')
+      if( error ) {
+        console.log('3')
+        return callback(error);
+      } else {
+        console.log('WOOOO Adding Player ', data);
+        return callback(null, data);
+      }
+    });
+  } else {
+    console.log('4')
+    return callback("Did not receive proper player data");
+  }
+}
+
 
 var playerIds = [];
 
 function populateDemoData() {
-  return function () {
-    var players = [
-      {
-        name : {
-          first : 'Seb',
-          last  : 'Donovor'
-        },
-        preferred_number : 54
+  var players = [
+    {
+      name : {
+        first : 'Seb',
+        last  : 'Donor'
       },
-      {
-        name : {
-          first : 'Duane',
-          last  : 'Goober'
-        },
-        preferred_number : 13
+      preferred_number : 54
+    },
+    {
+      name : {
+        first : 'Duane',
+        last  : 'Goober'
       },
-      {
-        name : {
-          first : 'Zack',
-          last  : 'Zebakraken'
-        },
-        preferred_number : 77
+      preferred_number : 13
+    },
+    {
+      name : {
+        first : 'Zack',
+        last  : 'Zebakraken'
       },
-      {
-        name : {
-          first : 'Shane',
-          last  : 'Kullmen'
-        },
-        preferred_number : 16
+      preferred_number : 77
+    },
+    {
+      name : {
+        first : 'Shane',
+        last  : 'Kullmen'
       },
-      {
-        name : {
-          first : 'Todd',
-          last  : 'Bootmaker'
-        },
-        preferred_number : 30
+      preferred_number : 16
+    },
+    {
+      name : {
+        first : 'Todd',
+        last  : 'Bootmaker'
       },
-      {
-        name : {
-          first : 'Gabe',
-          last  : 'Newell'
-        },
-        preferred_number : 69
+      preferred_number : 30
+    },
+    {
+      name : {
+        first : 'Gabe',
+        last  : 'Newell'
       },
-      {
-        name : {
-          first : 'John',
-          last  : 'Hasslehoff'
-        },
-        preferred_number : 15
+      preferred_number : 69
+    },
+    {
+      name : {
+        first : 'John',
+        last  : 'Hasslehoff'
       },
-      {
-        name : {
-          first : 'Charles',
-          last  : 'Orpik'
-        },
-        preferred_number : 44
+      preferred_number : 15
+    },
+    {
+      name : {
+        first : 'Charles',
+        last  : 'Orpik'
       },
-      {
-        name : {
-          first : 'Tony',
-          last  : 'Savere'
-        },
-        preferred_number : 39
+      preferred_number : 44
+    },
+    {
+      name : {
+        first : 'Tony',
+        last  : 'Savere'
       },
-      {
-        name : {
-          first : 'George',
-          last  : 'Goalie'
-        },
-        preferred_number : 1
-      }
-    ];
-    players.forEach(function (player) {
-      console.log('WOOOO')
-      Player.create(player, function(error, data){
-        if( !error ) {
-          console.log('WOOOO Adding Player ', data);
-        } else {
-          console.log('ERRR == ', error);
-        }
-      });
-    });
-  }
+      preferred_number : 39
+    },
+    {
+      name : {
+        first : 'George',
+        last  : 'Goalie'
+      },
+      preferred_number : 1
+    }
+  ];
+
+  console.log('OK')
+
+  async.map(players, createPlayer, function(err, results){
+    if ( err ) {
+      console.log('ERRO ' + err)
+
+
+      return "ERROR == " + err;
+    }
+
+    console.log('Result Count == ' + results.length);
+
+  });
+
+  console.log('OUT')
 }
