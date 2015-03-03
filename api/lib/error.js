48dd87d997cb61d4b627ada7a006fada1c0c86c2
John Hofrichter <john.hofrichter@gmail.com>
2015-03-03 01:00:28 -0500

    cooolllooooors

--- api/lib/error.js
+++ api/lib/error.js
@@ -86,7 +86,7 @@ exports.errorHandler = function (error, req, res, next) {
 
 
   function sendErr (err) {
-    console.log((err.status + ' - ' + (err.error || 'Could not process request')).red);
+    console.log(((err.status + '').red + ' - ' + (err.error || 'Could not process request').yellow));
     res.status(err.status || 500).send({
       error   : err.error || 'Could not process request',
       details : err.details || null
